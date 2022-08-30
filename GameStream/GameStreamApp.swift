0f2d05c2f4b361b0c4f35b18f7cbd7a363079b83
Adolfo Pardo <pardopatricioadolfo@icloud.com>
2022-08-30 15:54:38 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by adolfo pardo on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
