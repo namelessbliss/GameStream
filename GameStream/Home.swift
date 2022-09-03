c00c2601028d32147595dac4824ce43213037033
Adolfo Pardo <pardopatricioadolfo@icloud.com>
2022-09-03 17:10:13 -0500

    cambiar orden del la pestaña seleccionada Home

--- GameStream/Home.swift
+++ GameStream/Home.swift
@@ -8,33 +8,35 @@
 import SwiftUI
 
 struct Home: View {
+    
+    @State var tabSeleccionado:Int = 2
     var body: some View {
         
-        TabView{
+        TabView(selection: $tabSeleccionado){
             
             Text("Pantalla Perfil").font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem{
                     Image(systemName: "person")
                     Text("Perfil")
-                }
-            
-            Text("Pantalla Inicio").font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem{
-                    Image(systemName: "house")
-                    Text("Inicio")
-                }
+                }.tag(0)
             
             Text("Pantalla Juegos").font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem{
                     Image(systemName: "gamecontroller")
                     Text("Juegos")
-                }
+                }.tag(1)
+            
+            Text("Pantalla Inicio").font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem{
+                    Image(systemName: "house")
+                    Text("Inicio")
+                }.tag(2)
             
             Text("Pantalla Favoritos").font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem{
                     Image(systemName: "heart")
                     Text("Favoritos")
-                }
+                }.tag(3)
         }
     }
 }
