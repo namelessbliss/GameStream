58360d52e77af291141ab28638f92dbbcd9e1179
Adolfo Pardo <pardopatricioadolfo@icloud.com>
2022-09-03 15:33:54 -0500

    Creación de la Pantalla de Registro

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -90,7 +90,7 @@ struct InicioSesionView: View {
                         Text("ejemplo@gmail .com").font(.caption).foregroundColor(.gray)
                     }
                     
-                    TextField("", text: $correo)
+                    TextField("", text: $correo).foregroundColor(.white)
                 }
                 
                 Divider().frame(height: 1).background(Color("Dark-Cian")).padding(.bottom)
@@ -103,7 +103,7 @@ struct InicioSesionView: View {
                         Text("Escribe tu contraseña").font(.caption).foregroundColor(.gray)
                     }
                     
-                    SecureField("", text: $contraseña)
+                    SecureField("", text: $contraseña).foregroundColor(.white)
                 }
                 
                 Divider().frame(height: 1).background(Color("Dark-Cian")).padding(.bottom)
@@ -161,10 +161,141 @@ struct InicioSesionView: View {
 }
 
 struct RegistroView: View {
+    
+    @State var correo = ""
+    @State var contraseña = ""
+    @State var confirmarContraseña = ""
+    
     var body: some View{
         
-        Text("Soy la vista de Registro")
+        ScrollView {
+            
+            VStack(alignment: .center){
+                
+                Text("Elije una foto de perfil")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                Text("Puedes cambiar o elegirla más adelante")
+                    .font(.footnote)
+                    .fontWeight(.light)
+                    .foregroundColor(.gray)
+                    .padding(.bottom)
+                
+                Button(action: tomarFoto, label: {
+                    ZStack {
+                        Image("perfilEjemplo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                        
+                        Image(systemName: "camera").foregroundColor(.white)
+                    }
+                })
+                .padding(.bottom)
+            }
+            
+            
+            VStack{
+                
+                VStack(alignment: .leading){
+                    
+                    Text("Correo Electrónico*").foregroundColor(Color("Dark-Cian"))
+                        .frame(width: 300, alignment: .leading)
+                    
+                    ZStack(alignment: .leading){
+                        
+                        if correo.isEmpty {
+                            Text("ejemplo@gmail .com").font(.caption).foregroundColor(.gray)
+                        }
+                        
+                        TextField("", text: $correo).foregroundColor(.white)
+                    }
+                    
+                    Divider().frame(height: 1).background(Color("Dark-Cian")).padding(.bottom)
+                    
+                    Text("Contraseña").foregroundColor(.white)
+                    
+                    ZStack(alignment: .leading){
+                        
+                        if contraseña.isEmpty {
+                            Text("Escribe tu contraseña").font(.caption).foregroundColor(.gray)
+                        }
+                        
+                        SecureField("", text: $contraseña).foregroundColor(.white)
+                    }
+                    
+                    Divider().frame(height: 1).background(Color("Dark-Cian")).padding(.bottom)
+                    
+                    Text("Confirmar Contraseña").foregroundColor(.white)
+                    
+                    ZStack(alignment: .leading){
+                        
+                        if contraseña.isEmpty {
+                            Text("Vuelve a escribir tu contraseña").font(.caption).foregroundColor(.gray)
+                        }
+                        
+                        SecureField("", text: $confirmarContraseña).foregroundColor(.white)
+                    }
+                    
+                    Divider().frame(height: 1).background(Color("Dark-Cian")).padding(.bottom)
+                }
+                
+                Button(action: registrate, label: {
+                    Text("REGÍSTRATE")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                        .overlay(RoundedRectangle(cornerRadius: 6.0)
+                            .stroke(Color("Dark-Cian"), lineWidth: 1.0)
+                            .shadow(color: .white, radius: 6))
+                }).padding(.bottom,32)
+                
+                Text("Registrate con redes sociales")
+                    .foregroundColor(.white)
+                    .frame(width: 300, alignment: .center)
+                    .padding(.bottom)
+                
+                
+                HStack{
+                    Button(action: {print("Registro con Facebook")}) {
+                        Text("Facebook")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(.vertical, 12.0)
+                        //.background(Color(red: 36/255, green: 57/255, blue: 91/255, opacity: 1.0))
+                            .background(Color("Blue-Gray"))
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                    }
+                    
+                    Button(action: {print("Registro con Twitter")}) {
+                        Text("Twitter")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(.vertical, 12.0)
+                        //.background(Color(red: 36/255, green: 57/255, blue: 91/255, opacity: 1.0))
+                            .background(Color("Blue-Gray"))
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                    }
+                }
+                
+                
+            }.padding(.horizontal, 77)
+        }
     }
+}
+
+func tomarFoto(){
+    print("Tomar fotografía de perfil")
+}
+
+func registrate(){
+    print("Registro de usuario")
 }
 
 struct ContentView_Previews: PreviewProvider {
