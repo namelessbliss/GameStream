09af8bdd3452c83844f4413367bb6c5a310d1fe5
Adolfo Pardo <pardopatricioadolfo@icloud.com>
2022-09-04 17:12:30 -0500

    Añadiendo elementos recomendados y categorías en la pantalla Home con ScrollViews horizontales

--- GameStream/Home.swift
+++ GameStream/Home.swift
@@ -136,6 +136,121 @@ struct SubModuloHome:View{
                 
             }.frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
                 .padding(.vertical)
+            
+            Text("CTEGORÍAS SUGERIDAS PARA TI")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                
+                HStack{
+                    
+                    Button(action: {}, label: {
+                        
+                        ZStack{
+                            
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            
+                            Image("FPS")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                            
+                        }
+                        
+                    })
+                    
+                    Button(action: {}, label: {
+                        
+                        ZStack{
+                            
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            
+                            Image("RPG")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                            
+                        }
+                        
+                    })
+                    
+                    Button(action: {}, label: {
+                        
+                        ZStack{
+                            
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            
+                            Image("OpenWorld")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                            
+                        }
+                        
+                    })
+                }
+                
+            }
+            
+            Text("RECONMENDADOS PARA TI")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                
+                HStack{
+                    
+                    Button(action: {
+                        url = urlVideos[1]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    }, label: {
+                        
+                        Image("Abzu").resizable()
+                            .scaledToFit()
+                            .frame(width: 240, height: 135)
+                        
+                    })
+                    
+                    Button(action: {
+                        url = urlVideos[2]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    }, label: {
+                        
+                        Image("Crash Bandicoot").resizable()
+                            .scaledToFit()
+                            .frame(width: 240, height: 135)
+                        
+                    })
+                    
+                    Button(action: {
+                        url = urlVideos[3]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    }, label: {
+                        
+                        Image("DEATH STRANDING").resizable()
+                            .scaledToFit()
+                            .frame(width: 240, height: 135)
+                        
+                    })
+                    
+                }
+                
+            }
+            
         }
         
         NavigationLink(destination: VideoPlayer(player: AVPlayer(url: URL(string: url)!))
