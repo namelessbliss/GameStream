479c15514ef7c08b1fbf5c7869441d7c399be445
Adolfo Pardo <pardopatricioadolfo@icloud.com>
2022-09-05 22:14:04 -0500

    Creando modelo

--- GameStream/Modelos/Model.swift
+++ GameStream/Modelos/Model.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct Games:Codable {
+    
+    var games:[Game]
+    
+}
+
+struct Game:Codable{
+    
+    var title:String
+    var studio:String
+    var contentRating:String
+    var publicationYear:String
+    var description:String
+    var platforms:[String]
+    var tags:[String]
+    var videosUrls:videoUrl
+    var galleryImages:[String]
+    
+}
+
+struct videoUrl:Codable{
+    
+    var mobile:String
+    var tablet:String
+    
+}
