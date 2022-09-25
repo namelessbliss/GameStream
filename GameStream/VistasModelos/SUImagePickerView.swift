138308acbeed73417a7326930398d9e379eadf33
Adolfo Pardo <pardopatricioadolfo@icloud.com>
2022-09-25 17:41:44 -0500

    Modificación para guardar imagen en la memoria del dispositivo

--- GameStream/VistasModelos/SUImagePickerView.swift
+++ GameStream/VistasModelos/SUImagePickerView.swift
@@ -43,9 +43,30 @@ class ImagePickerViewCoordinator: NSObject, UINavigationControllerDelegate, UIIm
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            self.image = Image(uiImage: image)
+        
+        //Modificar nombre constante
+        
+        if let UiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            self.image = Image(uiImage: UiImage)
+            
+            if let data = UiImage.pngData(){
+                
+                let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+                
+                let url = documents.appendingPathComponent("fotoperfil.png")
+                
+                do {
+                    
+                    try data.write(to: url)
+                    
+                }catch{
+                    print("No pude guarda fotografía en el folder del dispositivo")
+                }
+                
+            }
         }
+        
         self.isPresented = false
     }
     
